58bc019d0aab2f66032d89fbdc0684f28e2afb79
Paweł Kowalski <kowalski.pawel.r@gmail.com>
2023-07-06 11:25:59 +0200

    fix SBC_NAME_FANCY

--- robots/panther/custom_config.sh
+++ robots/panther/custom_config.sh
@@ -12,4 +12,3 @@ cp ${CONFIG_FILES_PATH}/files/hwclock-set /lib/udev/hwclock-set
 # Setup envs
 echo "ROS_IP=10.15.20.3" >> /etc/environment
 echo "ROS_MASTER_URI=http://10.15.20.2:11311" >> /etc/environment
-echo "SBC_NAME_FANCY='Intel NUC'" >> /etc/environment
