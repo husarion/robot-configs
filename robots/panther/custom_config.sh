4347c21b45e0857a10f7b7db014eb4a192afcee7
Paweł Kowalski <82044322+pkowalsk1@users.noreply.github.com>
2023-07-06 12:02:22 +0200

    Add panther config (#1)

---
+++ robots/panther/custom_config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# If a particular robot requires any extra custom configuration steps, they should be included in this file.
+
+CONFIG_FILES_PATH="/etc/husarion/robot_configs/panther"
+
+# Time server config
+cp ${CONFIG_FILES_PATH}/files/chrony.conf /etc/chrony/chrony.conf
+cp ${CONFIG_FILES_PATH}/files/chrony.service /lib/systemd/system/chrony.service
+cp ${CONFIG_FILES_PATH}/files/hwclock-set /lib/udev/hwclock-set
+
+# Setup envs
+echo "ROS_IP=10.15.20.3" >> /etc/environment
+echo "ROS_MASTER_URI=http://10.15.20.2:11311" >> /etc/environment
