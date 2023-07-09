8437bff70cc98bd6183101e877a8ee9d1e36d8d8
dominikn <domini.nwk@gmail.com>
2023-07-09 16:04:30 +0000

    handling custom FastDDS and CycloneDDS urls

--- robots/rosbot_xl/ros2_humble/flash_firmware.sh
+++ robots/rosbot_xl/ros2_humble/flash_firmware.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Get the current user
+CURRENT_USER=$(whoami)
+
+# Check if the current user is "husarion"
+if [ "$CURRENT_USER" != "husarion" ]; then
+  echo "This script can only be run by the user 'husarion'."
+  exit 1
+fi
+
 # Define the Docker image
 DOCKER_IMAGE="husarion/rosbot-xl:humble"
 
@@ -9,17 +18,17 @@ RED='\033[0;31m'
 BOLD='\033[1m'
 NC='\033[0m' # No Color
 
-echo -e "${GREEN}[1/4]\tInitiating firmware flash on the ROSbot XL Digital Board. We're using the Docker image ${BOLD}$DOCKER_IMAGE${NC}${GREEN} for this process...${NC}"
+echo -e "${GREEN}[1/4]\r\nInitiating firmware flash on the ROSbot XL Digital Board. We're using the Docker image ${BOLD}$DOCKER_IMAGE${NC}${GREEN} for this process...${NC}"
 
 # Check if the Docker image exists
 if ! docker image inspect $DOCKER_IMAGE >/dev/null 2>&1; then
-  echo -e "\t${GREEN}${BOLD}$DOCKER_IMAGE${NC}${GREEN} not found. Pulling...${NC}"
+  echo -e "${GREEN}${BOLD}$DOCKER_IMAGE${NC}${GREEN} not found. Pulling...${NC}"
   docker pull $DOCKER_IMAGE
 fi
-echo -e "\t${GREEN}done${NC}"
+echo -e "${GREEN}done${NC}"
 
 # Stop the Docker containers if they're running
-echo -e "${GREEN}[2/4]\tStop ${BOLD}rosbot-xl${NC}${GREEN} and ${BOLD}microros${NC}${GREEN} Docker containers if they're running ${NC}"
+echo -e "\r\n${GREEN}[2/4]\r\nStop ${BOLD}rosbot-xl${NC}${GREEN} and ${BOLD}microros${NC}${GREEN} Docker containers if they're running ${NC}"
 
 # Define the Docker containers
 CONTAINERS=("rosbot-xl" "microros")
@@ -28,7 +37,7 @@ CONTAINERS=("rosbot-xl" "microros")
 for CONTAINER in "${CONTAINERS[@]}"; do
     CONTAINER_ID=$(docker ps -q -f name=$CONTAINER)
     if [ ! -z "$CONTAINER_ID" ]; then
-        echo "\tStopping container $CONTAINER..."
+        echo "Stopping container $CONTAINER..."
         docker stop $CONTAINER
     fi
 done
@@ -37,25 +46,25 @@ done
 DEVICE="/dev/ttyUSBDB"
 
 # Print a status update message, with the device name in bold
-echo -e "${GREEN}[3/4]\tChecking if the Digital Board (${BOLD}$DEVICE${NC}${GREEN}) is connected...${NC}"
+echo -e "\r\n${GREEN}[3/4]\r\nChecking if the Digital Board (${BOLD}$DEVICE${NC}${GREEN}) is connected...${NC}"
 
 # Use an if statement to check if the device file exists
 if [ -e "$DEVICE" ]; then
     # If the device file exists, print a confirmation message
-    echo -e "\t${GREEN}The device ${BOLD}$DEVICE${NC}${GREEN} is successfully detected.${NC}"
+    echo -e "${GREEN}The device ${BOLD}$DEVICE${NC}${GREEN} is successfully detected.${NC}"
 else
     # If the device file doesn't exist, print an error message and exit the script
-    echo -e "\t${RED}The device ${BOLD}$DEVICE${NC}${RED} cannot be detected. Please connect the Digital Board to a USB port.${NC}"
+    echo -e "${RED}The device ${BOLD}$DEVICE${NC}${RED} cannot be detected. Please connect the Digital Board to a USB port.${NC}"
     exit 1
 fi
 
 # Flashing
 
-echo -e "${GREEN}[4/4]\tFlashing the firmware...${NC}"
+echo -e "\r\n${GREEN}[4/4]\r\nFlashing the firmware...${NC}"
 
 docker run --rm -it --privileged \
 --mount type=bind,source=/dev/ttyUSBDB,target=/dev/ttyUSBDB \
 $DOCKER_IMAGE \
 flash-firmware.py -p /dev/ttyUSBDB
 
-echo -e "\t${GREEN}done${NC}"
+echo -e "${GREEN}done${NC}"
