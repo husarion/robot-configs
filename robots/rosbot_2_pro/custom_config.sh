d41f0adec76bb85258feae8d9b5cf2fe13e757a4
dominikn <dominik.nwk@gmail.com>
2023-08-11 15:16:37 +0200

    added config for ROSbot 2 PRO

---
+++ robots/rosbot_2_pro/custom_config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# If a particular robot requires any extra custom configuration steps, they should be included in this file.
+
+echo "no additional custom configuration for ROSbot 2R"
