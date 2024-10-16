4095e45fd93c0a98e748f0c401211ac2373199d8
Dawid Kmak <73443304+KmakD@users.noreply.github.com>
2024-10-16 09:49:59 +0200

    Install rosbot-xl snap

--- robots/rosbot_xl/custom_config.sh
+++ robots/rosbot_xl/custom_config.sh
@@ -2,4 +2,5 @@
 
 # If a particular robot requires any extra custom configuration steps, they should be included in this file.
 
-echo "no additional custom configuration for ROSbot XL"
+echo "Installing snap for ROSbot XL"
+sudo snap install rosbot-xl
