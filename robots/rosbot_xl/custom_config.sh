b60cf2a434faeffa744fa1d977087c1d583e2c68
kmakd <kmakd197@gmail.com>
2024-10-16 11:41:56 +0200

    add jazzy snap installation

--- robots/rosbot_xl/custom_config.sh
+++ robots/rosbot_xl/custom_config.sh
@@ -2,5 +2,10 @@
 
 # If a particular robot requires any extra custom configuration steps, they should be included in this file.
 
-echo "Installing snap for ROSbot XL"
-sudo snap install rosbot-xl
+echo "Installing ROSbot XL snap"
+
+if [ "$ROS_DISTRO" == "jazzy" ]; then
+    sudo snap install rosbot-xl --channel=jazzy
+else
+    sudo snap install rosbot-xl
+fi
