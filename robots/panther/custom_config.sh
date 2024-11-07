ca95f027d4a92357788fede7ecb7c18e93633804
kmakd <kmakd197@gmail.com>
2024-11-07 13:06:35 +0000

    add support for noetic

--- robots/panther/custom_config.sh
+++ robots/panther/custom_config.sh
@@ -9,5 +9,10 @@ cp ${CONFIG_FILES_PATH}/files/chrony.conf /etc/chrony/chrony.conf
 cp ${CONFIG_FILES_PATH}/files/chrony.service /lib/systemd/system/chrony.service
 cp ${CONFIG_FILES_PATH}/files/hwclock-set /lib/udev/hwclock-set
 
-# Set default rmw as cyclonedds
-echo "export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp" >> ~/.bashrc
+if [ "$ROS_DISTRO" == "noetic" ]; then
+  echo "ROS_IP=10.15.20.3" >>/etc/environment
+  echo "ROS_MASTER_URI=http://10.15.20.2:11311" >>/etc/environment
+else
+  # Set default rmw as cyclonedds
+  echo "export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp" >>~/.bashrc
+fi
