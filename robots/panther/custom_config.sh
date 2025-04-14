07a39a575a76b58789a9ec2291663e1db198b45b
dominikn <dominik.nwk@gmail.com>
2025-04-14 18:28:39 +0200

    small fix for panther

--- robots/panther/custom_config.sh
+++ robots/panther/custom_config.sh
@@ -2,12 +2,12 @@
 
 # If a particular robot requires any extra custom configuration steps, they should be included in this file.
 
-CONFIG_FILES_PATH="/etc/husarion/robot_configs/panther"
+SCRIPT_DIR="$(cd "$(dirname "$(readlink -f "$0")")" && pwd)"
 
 # Time server config
-cp ${CONFIG_FILES_PATH}/files/chrony.conf /etc/chrony/chrony.conf
-cp ${CONFIG_FILES_PATH}/files/chrony.service /lib/systemd/system/chrony.service
-cp ${CONFIG_FILES_PATH}/files/hwclock-set /lib/udev/hwclock-set
+cp ${SCRIPT_DIR}/files/chrony.conf /etc/chrony/chrony.conf
+cp ${SCRIPT_DIR}/files/chrony.service /lib/systemd/system/chrony.service
+cp ${SCRIPT_DIR}/files/hwclock-set /lib/udev/hwclock-set
 
 # Disable dhcp-server
 sudo service isc-dhcp-server stop
