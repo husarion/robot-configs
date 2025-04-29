5c3651c7a18140a58c0deaaffc507c28b7c386fd
dominikn <dominik.nwk@gmail.com>
2025-04-29 18:01:52 +0000

    fix factory reset for rosbot-xl humble

--- robots/rosbot_xl/ros2_humble/factory_reset.sh
+++ robots/rosbot_xl/ros2_humble/factory_reset.sh
@@ -31,7 +31,7 @@ done
 for snap in "${SNAP_LIST[@]}"; do
     echo "---------------------------------------"
     echo "Installing the \"$snap\" snap (ROS 2 ${ROS_DISTRO}/stable)"
-    sudo snap install "$snap" --channel="$ROS_DISTRO"/ --devmode
+    sudo snap install "$snap" --channel="$ROS_DISTRO"/stable
     sudo "$snap".stop
     sudo snap set "$snap" \
         ros.transport=udp-lo \
