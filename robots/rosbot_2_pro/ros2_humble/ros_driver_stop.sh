937e9770399a2285459c875b0106243df65bc852
rafal-gorecki <rafal.gorecki@husarion.com>
2025-07-01 11:47:27 +0200

    Update ros_driver_* script

--- robots/rosbot_2_pro/ros2_humble/ros_driver_stop.sh
+++ robots/rosbot_2_pro/ros2_humble/ros_driver_stop.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 sudo rosbot.stop
+sudo husarion-astra.stop
+sudo husarion-rplidar.stop
+sudo husarion-webui.stop
--- robots/rosbot_xl/ros2_humble/ros_driver_start.sh
+++ robots/rosbot_xl/ros2_humble/ros_driver_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo rosbot.start
-# sudo husarion-rplidar.start
 # sudo husarion-depthai.start
+# sudo husarion-rplidar.start
 sudo husarion-webui.start
