7122d0e181f1039de116a277a82cbf176684f1d6
rafal-gorecki <rafal.gorecki@husarion.com>
2025-07-04 10:28:02 +0200

    Remove jazzy from 2R

--- robots/rosbot_2r/jazzy/ros_driver_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo rosbot.start
-sudo husarion-astra.start
-sudo husarion-rplidar.start
-sudo husarion-webui.start
--- robots/rosbot_2r/jazzy/ros_driver_stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo rosbot.stop
-sudo husarion-astra.stop
-sudo husarion-rplidar.stop
-sudo husarion-webui.stop
