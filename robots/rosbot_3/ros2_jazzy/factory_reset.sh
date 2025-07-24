1d5dc99a4ebdb2342fef58e072f7a862a48fb204
Rafal Gorecki <126687345+rafal-gorecki@users.noreply.github.com>
2025-07-24 16:57:25 +0200

    Add ROSbot 3 factory_reset configuration (#27)

--- robots/rosbot_3/ros2_jazzy/factory_reset.sh
+++ robots/rosbot_3/ros2_jazzy/factory_reset.sh
@@ -6,8 +6,20 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 SNAP_LIST=(rosbot husarion-depthai husarion-rplidar husarion-webui)
 ROBOT_MODEL=rosbot
 LAYOUT_FILE="$SCRIPT_DIR/foxglove-rosbot.json"
+VALID_CONFIGURATIONS=("3" "3_pro")
+
+# Functions
+print_usage() {
+echo -e "\e[1mInvalid configuration.\e[0m"
+  echo "Usage: $0 <configuration>"
+  echo "Valid configurations:"
+  for config in "${VALID_CONFIGURATIONS[@]}"; do
+      echo "  - $config"
+  done  
+}
 
 # Source
+source "/etc/environment"
 if [ -f "$SCRIPT_DIR/../../helpers.sh" ]; then
     source "$SCRIPT_DIR/../../helpers.sh" # Working inside repo
 else
@@ -19,10 +31,29 @@ start_time=$(date +%s)
 
 check_user
 
+configuration="$1"
+
+if [[ -n "$configuration" ]]; then
+    if [[ " ${VALID_CONFIGURATIONS[@]} " =~ " ${configuration} " ]]; then
+        set_env "ROBOT_CONFIGURATION" "$configuration"
+    else
+        print_usage
+        exit 1
+    fi
+else
+    if [[ -n "$ROBOT_CONFIGURATION" ]]; then
+        echo -e "Default robot configuration '${ROBOT_CONFIGURATION}' will be used."
+        configuration="$ROBOT_CONFIGURATION"
+    else
+        print_usage
+        exit 1
+    fi
+fi
+
 print_header "Reinstall snaps"
 reinstall_snaps "${SNAP_LIST[@]}"
 
-print_header "Setting up ROSbot snap"
+print_header "Setting up rosbot snap for ROSbot $configuration"
 sudo /var/snap/rosbot/common/post_install.sh
 sudo snap set rosbot driver.robot-model=$ROBOT_MODEL
 sudo rosbot.flash
@@ -33,7 +64,13 @@ sudo snap set husarion-depthai driver.parent-frame=camera_mount_link
 
 print_header "Setting up RPLIDAR snap"
 sudo snap connect husarion-rplidar:shm-plug husarion-rplidar:shm-slot
-sudo snap set husarion-rplidar configuration=s2
+if [[ $configuration == "3" ]]; then
+    sudo snap set husarion-rplidar configuration=c1
+else if [[ $configuration == "3_pro" ]]; then
+    sudo snap set husarion-rplidar configuration=s2
+else
+    echo -e "\e[1mInvalid configuration for RPLIDAR.\e[0m"
+fi
 
 print_header "Setting up WebUI snap"
 sudo cp $LAYOUT_FILE /var/snap/husarion-webui/common/
